b67f59a5e4b2e3a0c0dcd4cf65fe6a85e7d7cb6c
xtreme-stevehiehn <shiehn@pivotal.io>
2017-06-14 15:05:55 -0400

    dddd

--- ci/build_source.sh
+++ ci/build_source.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd source-code
-gradle --no-daemon build
-mv build/libs/* ../../backend_build_output/
-cp manifest.yml ../../backend_build_output/manifest.yml
+gradle --no-daemon build 
+mv build/libs/* ../backend_build_output/
+cp manifest.yml ../backend_build_output/manifest.yml
