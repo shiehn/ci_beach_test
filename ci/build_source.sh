cac0733d8143b1037513cd41995c173236c57fd4
xtreme-stevehiehn <shiehn@pivotal.io>
2017-06-14 15:44:06 -0400

    dddd

--- ci/build_source.sh
+++ ci/build_source.sh
@@ -2,9 +2,5 @@
 
 cd source-code
 gradle --no-daemon build
-
-#cp build/libs/ci_beach_test-0.0.1-SNAPSHOT.jar ../backend_build_output/
-#cp manifest.yml ../backend_build_output/manifest.yml
-
-cd build/libs
-ls 
+mv build/libs/source-code-0.0.1-SNAPSHOT.jar ../backend_build_output/
+cp manifest.yml ../backend_build_output/manifest.yml
