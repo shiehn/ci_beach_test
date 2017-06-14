96c99fb810c74c948bf775637857a944853e0a2c
xtreme-stevehiehn <shiehn@pivotal.io>
2017-06-14 16:34:48 -0400

    fail commit

--- src/test/java/pivotal/io/citest/CitestApplicationTests.java
+++ src/test/java/pivotal/io/citest/CitestApplicationTests.java
@@ -5,12 +5,15 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static junit.framework.TestCase.fail;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CitestApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		fail("FAIL LOSER!!");
 	}
 
 }
