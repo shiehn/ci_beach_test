603e5ff2fe7201dc304bc143dd753e80f02921f6
xtreme-stevehiehn <shiehn@pivotal.io>
2017-06-15 14:12:46 -0400

    BlueGreen

--- src/test/java/pivotal/io/citest/CitestApplicationTests.java
+++ src/test/java/pivotal/io/citest/CitestApplicationTests.java
@@ -3,6 +3,7 @@ package pivotal.io.citest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.IfProfileValue;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static junit.framework.TestCase.fail;
@@ -15,5 +16,4 @@ public class CitestApplicationTests {
 	public void contextLoads() {
 
 	}
-
 }
---
+++ src/test/java/pivotal/io/citest/SmokeTests.java
@@ -0,0 +1,39 @@
+package pivotal.io.citest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.IfProfileValue;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@IfProfileValue(name="testprofile", value="smoketests")
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmokeTests {
+
+
+
+    @Test
+    public void shouldReturn20OkWhenDeployed() throws IOException {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+
+        String colour = Files.lines(Paths.get("current-app-info/next-app.txt")).findFirst().get();
+        String suffix  = System.getenv("PWS_APP_SUFFIX");
+        String domain  = System.getenv("PWS_APP_DOMAIN");
+
+        ResponseEntity<Void> response = restTemplate.getForEntity(String.format("http://%s-%s.%s", colour, suffix, domain), Void.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+}
